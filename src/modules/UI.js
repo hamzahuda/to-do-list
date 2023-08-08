5c278319a93cc826aff23fc487bea09c7940a998
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-08-08 10:44:22 +0100

    Create Add Project Modal

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,4 +1,29 @@
-import { projects } from "./projects";
+import { projects, createProject } from "./projects";
+
+export function loadAddProjectButton() {
+    const addProject = document.getElementById("addProject");
+    const projectModal = document.getElementById("projectModal");
+    const confirmBtn = projectModal.querySelector("#confirmBtn");
+    const newProjectTitle = document.getElementById("newProjectTitle");
+
+    addProject.addEventListener("click", () => {
+        projectModal.showModal();
+        // TODO: Darken background on modal opening
+    });
+
+    // Confirm button shouldn't submit but does reset form
+    // Close button has formmethod="dialog", form data stays and modal closes
+    confirmBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (newProjectTitle.value !== "") {
+            createProject(newProjectTitle.value);
+            loadProjectList();
+            loadToDoList(projects.length - 1);
+            newProjectTitle.value = "";
+        }
+        projectModal.close();
+    });
+}
 
 export function loadProjectList() {
     const projectList = document.getElementById("projectList");
